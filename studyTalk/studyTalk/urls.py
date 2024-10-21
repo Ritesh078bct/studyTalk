da1ddb7bbdb2044ba57107eb6c06203961099db6
Ritesh078bct <078bct068@student.ioepc.edu.np>
2024-10-21 09:39:45 +0545

    configured tailwind

--- studyTalk/studyTalk/urls.py
+++ studyTalk/studyTalk/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
